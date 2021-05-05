d5fdf1fba673bd3553c202a0541700b3514ae079
Prafulla Pol <prafulla.pol@sailpoint.com>
2021-05-05 18:53:38 +0530

    Adding Jquery required by bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { title } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-todo-app';
+
+  constructor() {
+    // setTimeout(() => {
+    //   this.title = 'Changed in the setTimeout';
+    // }, 2000);
+  }
 }
